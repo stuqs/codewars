fc5a1a652c33cff38b98e95d3be3b5b3345d3d31
Oleksandr Kozarezniuk <okozarezniuk@eisgroup.com>
2023-09-07 18:06:20 +0400

    improve performance

--- src/Walking_in_the_NASA_facility/NASAFacility.java
+++ src/Walking_in_the_NASA_facility/NASAFacility.java
@@ -2,101 +2,110 @@ package Walking_in_the_NASA_facility;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.OptionalInt;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class NASAFacility {
     private static final String WALKERS = "udlr";
     private static final Pattern PATTERN_RIGHT_LEFT = Pattern.compile("r(--)*l");
-    private static final Pattern PATTERN_UP_DOWN = Pattern.compile("d(--)*u");
+    private static final Pattern PATTERN_DOWN_UP = Pattern.compile("d(--)*u");
 
     public static int collision(char[][] room) {
-        String[][] path = copyArrayLength(room);
-        for (int i = 0; i < room.length; i++) {
-            for (int j = 0; j < room[i].length; j++) {
-                calculatePath(String.valueOf(room[i][j]), i, j, path);
-            }
+        int passingValue = passingCheck(room);
+        //as 1 is a minimum result
+        if (passingValue == 1) {
+            return passingValue;
         }
-        int result = Integer.min(processPath(path), passingCheck(room));
+
+        String[][] path = copyArrayLength(room);
+        calculatePath(room, path);
+        int result = Integer.min(processPath(path), passingValue);
         return result == Integer.MAX_VALUE ? -1 : result;
     }
 
-    private static void calculatePath(String s, int i, int j, String[][] path) {
-        if (!WALKERS.contains(s)) {
-            return;
-        }
-        int count = 0;
-        switch (s) {
-            case "u" -> {
-                for (int k = i; k >= 0; k--) {
-                    path[k][j] = Objects.requireNonNullElse(path[k][j], "") + count + ":";
-                    count++;
+    private static void calculatePath(char[][] room, String[][] path) {
+        for (int i = 0; i < room.length; i++) {
+            for (int j = 0; j < room[i].length; j++) {
+                String s = String.valueOf(room[i][j]);
+
+                if (!WALKERS.contains(s)) {
+                    continue;
+                }
+                int count = 0;
+                try {
+                    switch (s) {
+                        case "u" -> {
+                            for (int k = i; k >= 0; k--) {
+                                path[k][j] = Objects.requireNonNullElse(path[k][j], "") + count + ":";
+                                count++;
+                            }
+                        }
+                        case "d" -> {
+                            for (int k = i; k < path.length; k++) {
+                                path[k][j] = Objects.requireNonNullElse(path[k][j], "") + count + ":";
+                                count++;
+                            }
+                        }
+                        case "l" -> {
+                            for (int k = j; k >= 0; k--) {
+                                path[i][k] = Objects.requireNonNullElse(path[i][k], "") + count + ":";
+                                count++;
+                            }
+                        }
+                        case "r" -> {
+                            for (int k = j; k < path[i].length; k++) {
+                                path[i][k] = Objects.requireNonNullElse(path[i][k], "") + count + ":";
+                                count++;
+                            }
+                        }
+                        default -> throw new IllegalArgumentException("Input parameter was wrong: " + s);
+                    }
+                } catch (IndexOutOfBoundsException ignored) {
                 }
             }
-            case "d" -> {
-                for (int k = i; k < path.length; k++) {
-                    path[k][j] = Objects.requireNonNullElse(path[k][j], "") + count + ":";
-                    count++;
-                }
-            }
-            case "l" -> {
-                for (int k = j; k >= 0; k--) {
-                    path[i][k] = Objects.requireNonNullElse(path[i][k], "") + count + ":";
-                    count++;
-                }
-            }
-            case "r" -> {
-                for (int k = j; k < path[i].length; k++) {
-                    path[i][k] = Objects.requireNonNullElse(path[i][k], "") + count + ":";
-                    count++;
-                }
-            }
-            default -> throw new IllegalArgumentException("Input parameter was wrong: " + s);
         }
     }
 
     private static int passingCheck(char[][] room) {
-        return Stream.of(passingVerticalCheck(room), passingHorizontalCheck(room))
-                .flatMap(Collection::stream)
-                .mapToInt(Integer::intValue)
+        return Stream.of(passingHorizontalCheck(room), passingVerticalCheck(room))
+                .filter(OptionalInt::isPresent)
+                .mapToInt(OptionalInt::getAsInt)
                 .min()
                 .orElse(Integer.MAX_VALUE);
     }
 
-    private static List<Integer> passingHorizontalCheck(char[][] room) {
-        List<Integer> result = new ArrayList<>();
-        for (char[] row : room) {
-            int lengthPath = PATTERN_RIGHT_LEFT.matcher(joinChars(row)).results()
-                    .mapToInt(r -> r.group().length() / 2)
-                    .min()
-                    .orElse(Integer.MAX_VALUE);
-            result.add(lengthPath);
-        }
-        return result;
+    private static OptionalInt passingHorizontalCheck(char[][] room) {
+        return Arrays.stream(room)
+                .map(String::new)
+                .flatMap(s -> PATTERN_RIGHT_LEFT.matcher(s).results())
+                .mapToInt(r -> r.group().length() / 2)
+                .min();
     }
 
-    private static List<Integer> passingVerticalCheck(char[][] room) {
-        char[][] inverted = invertedArray(room);
-        for (int i = 0; i < room.length; i++) {
-            for (int j = 0; j < room[i].length; j++) {
-                inverted[j][i] = room[i][j];
+    private static OptionalInt passingVerticalCheck(char[][] room) {
+        int maxRowLength = Arrays.stream(room)
+                .mapToInt(a -> a.length)
+                .max()
+                .orElse(0);
+        List<String> columns = new ArrayList<>();
+        for (int i = 0; i < maxRowLength; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (char[] chars : room) {
+                try {
+                    sb.append(chars[i]);
+                } catch (IndexOutOfBoundsException e) {
+                    sb.append("x");
+                }
             }
+            columns.add(sb.toString());
         }
-
-        List<Integer> result = new ArrayList<>();
-        for (char[] column : inverted) {
-            int lengthPath = PATTERN_UP_DOWN.matcher(joinChars(column)).results()
-                    .mapToInt(r -> r.group().length() / 2)
-                    .min()
-                    .orElse(Integer.MAX_VALUE);
-            result.add(lengthPath);
-        }
-        return result;
+        return columns.stream()
+                .flatMap(s -> PATTERN_DOWN_UP.matcher(s).results())
+                .mapToInt(r -> r.group().length() / 2)
+                .min();
     }
 
     private static int processPath(String[][] path) {
@@ -110,21 +119,14 @@ public class NASAFacility {
                 for (String pair : movements) {
                     if (Arrays.stream(movements)
                             .filter(d -> d.equals(pair))
-                            .count() > 1
-                            && result > Integer.parseInt(pair)) {
-                        result = Integer.parseInt(pair);
+                            .count() > 1) {
+                        int parsedValue = Integer.parseInt(pair);
+                        result = Integer.min(result, parsedValue);
                     }
                 }
             }
         }
         return result;
-    }
-
-    private static String joinChars(char[] array) {
-        return IntStream.range(0, array.length)
-                .mapToObj(i -> array[i])
-                .map(Object::toString)
-                .collect(Collectors.joining());
     }
 
     private static String[][] copyArrayLength(char[][] array) {
@@ -133,23 +135,5 @@ public class NASAFacility {
             copy[i] = new String[array[i].length];
         }
         return copy;
-    }
-
-    private static char[][] invertedArray(char[][] room) {
-        int maxRowLength = 0;
-        for (char[] ints : room) {
-            maxRowLength = Integer.max(ints.length, maxRowLength);
-        }
-        char[][] inverted = new char[maxRowLength][room.length];
-        for (char[] chars : inverted) {
-            Arrays.fill(chars, '-');
-        }
-
-        for (int i = 0; i < room.length; i++) {
-            for (int j = 0; j < room[i].length; j++) {
-                inverted[j][i] = room[i][j];
-            }
-        }
-        return inverted;
     }
 }
