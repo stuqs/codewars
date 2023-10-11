57c8e45909d56808cdf76f2e31a8cba7c82872d4
Oleksandr Kozarezniuk <okozarezniuk@eisgroup.com>
2023-10-11 13:41:28 +0700

    Test refactor

--- src/Break_the_pieces/BreakPiecesTest.java
+++ src/Break_the_pieces/BreakPiecesTest.java
@@ -2,38 +2,43 @@ package Break_the_pieces;
 
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 
 public class BreakPiecesTest {
+    private static final Map<String[], String[]> DATA = Map.ofEntries(
+            Map.entry(
+                    new String[]{
+                            "Test 1",
+                            "+------------+\n|            |\n|            |\n|            |\n+------+-----+\n|      |     |\n|      |     |\n+------+-----+"
+                    },
+                    new String[]{
+                            "+------------+\n|            |\n|            |\n|            |\n+------------+",
+                            "+------+\n|      |\n|      |\n+------+",
+                            "+-----+\n|     |\n|     |\n+-----+"
+                    }),
+
+            Map.entry(
+                    new String[]{
+                            "Test 2",
+                            "    +---+\n    |   |\n+---+   |\n|       |\n+-------+"
+                    },
+                    new String[]{
+                            "    +---+\n    |   |\n+---+   |\n|       |\n+-------+"
+                    }
+            )
+
+    );
+
     @Test
     public void simpleTest() {
-        String shape = String.join("\n", new String[]{"+------------+",
-                "|            |",
-                "|            |",
-                "|            |",
-                "+------+-----+",
-                "|      |     |",
-                "|      |     |",
-                "+------+-----+"});
-        String expected[] = {String.join("\n", new String[]{"+------------+",
-                "|            |",
-                "|            |",
-                "|            |",
-                "+------------+"}),
-                String.join("\n", new String[]{"+------+",
-                        "|      |",
-                        "|      |",
-                        "+------+"}),
-                String.join("\n", new String[]{"+-----+",
-                        "|     |",
-                        "|     |",
-                        "+-----+"})};
-        String actual[] = BreakPieces.process(shape);
-        Arrays.sort(expected);
-        Arrays.sort(actual);
-        assertEquals(expected, actual);
+        for (Map.Entry<String[], String[]> e : DATA.entrySet()) {
+            System.out.println("Processing test - " + e.getKey()[0]);
+            String[] actual = BreakPieces.process(e.getKey()[1]);
+            String[] expected = e.getValue();
+            assertEquals(expected, actual);
+        }
     }
 }
